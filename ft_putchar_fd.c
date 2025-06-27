d4cbd410566d16899e7621a6b2b77de153e448b4
MrY2K <ayoubsamoraizi@gmail.com>
2025-06-27 17:05:05 +0100

    improved_atoi added

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -5,6 +5,7 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: achoukri <achoukri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
+<<<<<<<< HEAD:_CC/C III/lib/ft_putchar_fd.c
 /*   Created: 2024/11/01 21:44:40 by achoukri          #+#    #+#             */
 /*   Updated: 2025/04/24 10:23:11 by achoukri         ###   ########.fr       */
 /*                                                                            */
@@ -16,3 +17,16 @@ void	ft_putchar_fd(char c, int fd)
 {
 	write (fd, &c, 1);
 }
+========
+/*   Created: 2025/04/14 13:42:45 by achoukri          #+#    #+#             */
+/*   Updated: 2025/04/30 22:37:23 by achoukri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("HEY");
+}
+>>>>>>>> 28d0690 (Philo Day 0):_CC/C III/Philosophers/philo/src/main.c
